ecf49c2d8e593a9b782d9e54fe7ea182b86b95c1
FatemaLokat <52966732+FatemaLokat@users.noreply.github.com>
2020-05-27 23:24:13 +0530

    Add files via upload

---
+++ database/migrations/2020_05_03_143706_create_teachers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeachersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('Teacher_ID');
+            $table->string('Name');
+            $table->string('Book_ID');
+            $table->string('Department');
+            $table->string('Email_ID');
+            $table->string('Mobile_Number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teachers');
+    }
+}
